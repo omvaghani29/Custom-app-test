461fbc3358d894b59d7794205ba1d77a293aa044
omVaghani <vishal.dave@plusinfosys.com>
2025-12-29 18:22:50 +0530

    Change the url

--- vite.config.ts
+++ vite.config.ts
@@ -41,6 +41,7 @@ export default defineConfig({
     cors: {
       preflightContinue: true,
     },
+    host: "0.0.0.0",
     port: Number(process.env.PORT || 3000),
     hmr: hmrConfig,
     fs: {
